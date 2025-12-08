5fee2b613b36543bc1dde27437114e3e6a8f6264
olivvein <parcouru_epoque.9b@icloud.com>
2025-12-08 13:23:26 +0100

    no console log

--- services/physicsEngine.ts
+++ services/physicsEngine.ts
@@ -797,7 +797,6 @@ export const updatePhysics = (
         if (body.landedOnBodyId) {
             // Check if we are applying thrust to takeoff
             const isThrusting = body.thrust && (Math.abs(body.thrust.x) > 0.0001 || Math.abs(body.thrust.y) > 0.0001);
-            console.log("isThrusting:", isThrusting);
             
             if (isThrusting) {
                 body.landedOnBodyId = undefined;
